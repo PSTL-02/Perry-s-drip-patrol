107c58a214d8e0b9a4f901a9fa58860669be46a2
PSTL-02 <tbnrneelplayz@gmail.com>
2024-09-13 09:25:42 +1200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+// DotEnV
+require('dotenv').config()
+// Cors
+const cors = require('cors');
+// Bring in Express
+const express = require('express');
+// mongoose:
+const mongoose = require('mongoose');
+// set a variable of app to run the express method
+const app = express();
+// set a port - listen changes on the port
+const port = 4000;
+// allow Cross Origin
+app.use(cors());
+
+// use json with express
+app.use(express.json());
+
+// import routes
+
+
+// Attach our route to our app (express)
+
+
+// Serve static files from public/uploads
+
+
+
+//log out path and method of each request:
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+const mongoUsername = process.env.MONGODB_USERNAME
+const mongoPassword = process.env.MONGODB_PASSWORD
+
+const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.evt4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+// Define the home route for the backend
+app.get('/', (req, res) => {
+    // what happens at that route
+    res.send("Hello, this is your express server!");
+});
+
+// Listen to changes
+app.listen(port, () => {
+    console.log(`Express server is running on http://localhost:${port}`);
+});
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB Atlas:', err);
+    });
---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
