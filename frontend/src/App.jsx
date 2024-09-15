bf791416f75c9ab82e46c6cabd0113152464f44f
Mere Frances <meredithflss@gmail.com>
2024-09-16 11:33:20 +1200

    correct authcontext errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 import Navbar from './components/Navbar'
 
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 const App = () => {
+  const {user} = useAuthContext();
   
   return (
     <>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/login' element={user ? <Login/> : <Navigate to='/login'/>}/>
+        <Route path='/signup' element={user ? <SignUp/> : <Navigate to='/signup'/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
